a35a3299558335327c3e2358d765abfe23c86154
Snir Shechter <snirshechter@gmail.com>
2017-07-04 18:01:09 +0300

    added vuex and store state

---
+++ Client/src/components/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="register">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'register',
+  data() {
+    return {
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
